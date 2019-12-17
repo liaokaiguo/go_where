7dfc35b7814fd72ffb4383d87140557c2a17d436
liaokaiguo <1245952876@qq.com>
2019-12-17 15:38:25 +0800

    城市页滚动和右侧导航

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <ul class="list">
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+	export default {
+		name: "CityAlphabet"
+	}
+</script>
+
+<style lang="stylus"scoped>
+  @import "~styles/varibles.styl"
+  .list
+    /*垂直居中*/
+    display :flex
+    flex-direction :column
+    justify-content :center
+    text-align :center
+    position :absolute
+    top:2.77rem
+    right :0
+    bottom :0
+    width :.4rem
+    .item
+      text-align :center
+      line-height :.4rem
+      color:$bgColor
+</style>
