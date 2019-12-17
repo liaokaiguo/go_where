0c6d84f34b0d7623d1c78bdedb196d37d7f3a4c9
liaokaiguo <1245952876@qq.com>
2019-12-17 16:22:00 +0800

    City_ajax数据

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :hotcities="hotCities" :cities="cities"></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
   </div>
 </template>
 
@@ -12,13 +12,36 @@
   import CitySearch from "./components/Search";
   import CityList from "./components/List";
   import CityAlphabet from "./components/Alphabet";
+  import axios from 'axios'
   export default {
 		name: "City",
+    data(){
+			return{
+				hotCities:[],
+        cities:{}
+      }
+    } ,
 	  components: {
 	    CityAlphabet,
 	    CityList,
 			CitySearch,
       CityHeader,
+    },
+    mounted() {
+      this.getCityInfo()
+    },
+    methods:{
+			getCityInfo(){
+        axios.get('/api/city.json')
+            .then(this.handleGetCityInfoSucc)
+      },
+      handleGetCityInfoSucc(res){
+				res = res.data
+        if(res.ret && res.data){
+          this.hotCities = res.data.hotCities
+          this.cities = res.data.cities
+        }
+      }
     }
   }
 </script>
