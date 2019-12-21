892232260852fffc480dcf669579e42329aeaf21
liaokaiguo <1245952876@qq.com>
2019-12-21 20:57:47 +0800

    细节修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/Home'
-import City from '@/pages/city/City'
-import Detail from '@/pages/detail/Detail'
+// 异步组件使用
+// import Home from '@/pages/home/Home'
+// import City from '@/pages/city/City'
+// import Detail from '@/pages/detail/Detail'
 
 Vue.use(Router)
 
@@ -11,16 +12,16 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home,
+      component: () => import('@/pages/home/Home'),
     },{
       path: '/City',
       name: 'City',
-      component: City,
+      component: () => import('@/pages/city/City')
     },
     {
       path:'/detail/:id',
       name:'Detail',
-      component:Detail
+      component:() => import('@/pages/detail/Detail')
     }
   ],
   scrollBehavior(to, from, savePositon){
